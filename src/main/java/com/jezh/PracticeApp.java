ee7530c3fe44b48b6313509d3f564a911080bb41
jezhische <jezhische@gmail.com>
2017-11-04 09:51:59 +0200

    some changes

--- src/main/java/com/jezh/PracticeApp.java
+++ src/main/java/com/jezh/PracticeApp.java
@@ -1,12 +1,5 @@
 package com.jezh;
 
-import com.jezh.base.Track;
-import com.jezh.baseImpl.TrackImpl;
-import com.jezh.entity.Vehicle;
-import com.jezh.entityImpl.HeavyTruck;
-import com.jezh.entityImpl.LightCar;
-import com.jezh.race.Race;
-import com.jezh.raceImpl.RaceImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PracticeApp {
